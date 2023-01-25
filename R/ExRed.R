2329a0bc2d136e1c96f9fba23cbe62cd30885938
huntercole25 <53839906+huntercole25@users.noreply.github.com>
2023-01-25 09:45:36 -0500

    Adding LandsatQualityCodes function, and associated data and documentation. Additionally, fixing ClassFinder function.

---
+++ R/ExRed.R
@@ -0,0 +1,12 @@
+#' Landsat 8 Band 4 Imagery from San Clemente Island, CA 2015-11-27
+#'
+#' A raster representing Lansat 8 (OLI/TIRS) Collection 2 Tier 1 Band 4 data taken on November 27, 2015.
+#' The data has been cropped to the extent 352244, 3648387:356562, 3653420 (EPSG:32611).
+#'
+#' @docType data
+#'
+#' @usage data(ExRed)
+#'
+#' @format A `raster` class object.
+#'
+'ExRed'
